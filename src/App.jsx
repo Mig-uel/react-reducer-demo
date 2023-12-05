f6f6dd673061f10a11db09d1d01680381d9b6005
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-05 18:52:09 -0500

    Add useReducer hook to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useReducer } from 'react'
 import './App.css'
 
 function App() {
+  // state object, function to alter state
+  const [state, dispatch] useReducer()
+
   return <></>
 }
 
