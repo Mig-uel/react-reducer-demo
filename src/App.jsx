fe6cd03655cdc7e3c8dee4adbac2db93d21d3422
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-05 19:25:48 -0500

    Add state management using useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,67 @@
 import { useReducer } from 'react'
 import './App.css'
 
-function App() {
-  // state object, function to alter state
-  const [state, dispatch] useReducer()
+const reducer = (state, action) => {
+  // can destructure state and action
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + 1 }
+    case 'decrement':
+      return { ...state, count: state.count - 1 }
+    case 'newUserInput':
+      return { ...state, userInput: action.payload }
+    case 'toggleColor':
+      return { ...state, color: !state.color }
+    default:
+      throw new Error()
+  }
+}
 
-  return <></>
+function App() {
+  // state object, function to dispatch/send an action to alter state
+  // destructuring count, userInput and color from 'state' var
+  const [{ count, userInput, color }, dispatch] = useReducer(reducer, {
+    count: 0,
+    userInput: '',
+    color: false,
+  })
+
+  const handleClick = ({ target: { name } }) => {
+    if (name === 'increment') dispatch({ type: name })
+    else dispatch({ type: name })
+  }
+
+  return (
+    <main style={{ color: color ? '#FFF' : '#FFF952' }}>
+      {userInput && <h1>{userInput}</h1>}
+      <input
+        type='text'
+        value={userInput}
+        onChange={
+          (
+            { target: { value } } // destructuring 'e' event object
+          ) => dispatch({ type: 'newUserInput', payload: value }) // payload is the e.target.value
+        }
+      />
+      <br />
+      <h1>
+        <strong>{count}</strong>
+      </h1>
+      <span>
+        <button onClick={handleClick} name='increment'>
+          +
+        </button>
+        <button onClick={handleClick} name='decrement'>
+          -
+        </button>
+        <div>
+          <button onClick={() => dispatch({ type: 'toggleColor' })}>
+            Toggle Color
+          </button>
+        </div>
+      </span>
+    </main>
+  )
 }
 
 export default App
